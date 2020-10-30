961550a4e8f651a863e25dce8d6bf2b910055f4c
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-10-30 22:13:06 +0200

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+# https://fastapi.tiangolo.com
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "hello, world!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
